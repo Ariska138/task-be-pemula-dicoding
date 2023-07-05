5c665c3a7479ffc72bf6157d890f22ffb4968ac3
Ariska Hidayat <ariska138@gmail.com>
2023-07-05 15:43:49 +0700

    route

--- src/server.js
+++ src/server.js
@@ -10,22 +10,38 @@ const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
   response.statusCode = 200;
 
-  const { method } = request;
+  const { url, method } = request;
 
-  if (method === 'GET') {
-    response.end('<h1>Hello!</h1>');
-  }
+  if (url === '/') {
+    if (method === 'GET') {
+      response.end('<h1>Hello!</h1>');
+    }
 
-  if (method === 'POST') {
-    response.end('<h1>Hai!</h1>');
-  }
+    if (method === 'POST') {
+      let body = [];
 
-  if (method === 'PUT') {
-    response.end('<h1>Bonjour!</h1>');
-  }
+      request.on('data', (chunk) => {
+        body.push(chunk);
+      });
 
-  if (method === 'DELETE') {
-    response.end('<h1>Salam!</h1>');
+      request.on('end', () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.end(`<h1>Hai, ${name}!</h1>`);
+      });
+    }
+
+    if (method === 'PUT') {
+      response.end('<h1>Bonjour!</h1>');
+    }
+
+    if (method === 'DELETE') {
+      response.end('<h1>Salam!</h1>');
+    }
+  } else if (url === '/about') {
+    // TODO 3: logika respons bila url bernilai '/about'
+  } else {
+    // TODO 1: logika respons bila url bukan '/' atau '/about'
   }
 };
 
